4572a90400629dc08ee275a6a541c05df357a2d2
Benjamin Stauffer <kcowolf@gmail.com>
2020-05-18 22:52:40 -0500

    Simplify file structure

--- inc/GAME.h
+++
@@ -1,8 +0,0 @@
-#ifndef GAME_H
-#define GAME_H
-
-#define GAME_BKG1_MBANK 21
-
-void GAME_init();
-
-#endif
--- src/main.c
+++ src/main.c
@@ -1,3 +1,6 @@
+// Writing tilemap to the foreground layer (layer 1) works.  Writing it to background layer (layer 0) does not.
+// (See initTileMap function for where this is written).
+
 #include <cx16.h>
 #include <stdio.h>
 #include <stdint.h>
@@ -5,13 +8,49 @@
 #include "load.h"
 #include "vload.h"
 
-#include "GAME.h"
 #include "GFX.h"
-#include "GFX_util.h"
 #include "GFX_vera.h"
 
+// VERA Memory map
+// 0x0000-0x0FFF background tilemap (64 x 32 x 2 = 4096 bytes)
+// 0x1000-0x1FFF foreground tilemap (64 x 32 x 2 = 4096 bytes)
+// 0x2000-0x9FFF background tiles (1024 tiles * 32 bytes per tile = 32768 bytes)
+// 0xA000-0x11FFF foreground tiles (1024 tiles * 32 bytes per tile = 32768 bytes)
+// 0x12000-0x1F9BF unused
+
+#define BACKGROUND_MAP_BASE_ADDR 0x0
+#define FOREGROUND_MAP_BASE_ADDR 0x1000
+#define BACKGROUND_TILE_BASE_ADDR 0x2000
+#define FOREGROUND_TILE_BASE_ADDR 0xA000
+
+#define PALETTE_ADDR 0x1FA00
+
+#define PLANE_WIDTH 64
+#define PLANE_WIDTH_TIMES_TWO 128
+#define PLANE_HEIGHT 32
+
+#define BANKED_RAM_START 0xA000
+#define BKG1_MBANK 21
+
+void initTileMap();
+
 void initVera();
+
 int loadResources();
+
+void setBackgroundTile
+    (
+    uint8_t x,
+    uint8_t y,
+    uint16_t tile
+    );
+
+void setForegroundTile
+    (
+    uint8_t x,
+    uint8_t y,
+    uint16_t tile
+    );
 
 int main()
 {
@@ -22,9 +61,29 @@ int main()
 
     initVera();
 
-    GAME_init();
+    initTileMap();
 
 	return 0;
+}
+
+void initTileMap()
+{
+	uint8_t x = 0;
+    uint8_t y = 0;
+    uint16_t* tilemap = (uint16_t*)BANKED_RAM_START;
+
+    // Write background tilemap to VRAM
+    VIA1.pra = BKG1_MBANK;
+
+    for (y = 0; y < TMBG1_TILE_HEIGHT; y++)
+	{
+        for (x = 0; x < TMBG1_TILE_WIDTH; x++)
+		{
+            uint16_t tile = tilemap[(y * TMBG1_TILE_WIDTH) + x];
+			setBackgroundTile(x, y, tile);  // Doesn't work -- black screen
+            //setForegroundTile(x, y, tile);  // Works as expected -- draws the image
+        }
+	}
 }
 
 void initVera()
@@ -40,14 +99,14 @@ void initVera()
     VERA.display.vscale = VERA_SCALE_1x;
 
     VERA.layer0.config = VERA_CONFIG_MAP_WIDTH_64 | VERA_CONFIG_MAP_HEIGHT_32 | VERA_CONFIG_4BPP;
-    VERA.layer0.mapbase = GFX_BACKGROUND_MAP_BASE_ADDR >> 9;
-    VERA.layer0.tilebase = GFX_BACKGROUND_TILE_BASE_ADDR >> 9 | VERA_TILE_HEIGHT_8 | VERA_TILE_WIDTH_8;
+    VERA.layer0.mapbase = BACKGROUND_MAP_BASE_ADDR >> 9;
+    VERA.layer0.tilebase = BACKGROUND_TILE_BASE_ADDR >> 9 | VERA_TILE_HEIGHT_8 | VERA_TILE_WIDTH_8;
     VERA.layer0.hscroll = 0;
     VERA.layer0.vscroll = 0;
 
     VERA.layer1.config = VERA_CONFIG_MAP_WIDTH_64 | VERA_CONFIG_MAP_HEIGHT_32 | VERA_CONFIG_4BPP;
-    VERA.layer1.mapbase = GFX_FOREGROUND_MAP_BASE_ADDR >> 9;
-    VERA.layer1.tilebase = GFX_FOREGROUND_TILE_BASE_ADDR >> 9 | VERA_TILE_HEIGHT_8 | VERA_TILE_WIDTH_8;
+    VERA.layer1.mapbase = FOREGROUND_MAP_BASE_ADDR >> 9;
+    VERA.layer1.tilebase = FOREGROUND_TILE_BASE_ADDR >> 9 | VERA_TILE_HEIGHT_8 | VERA_TILE_WIDTH_8;
     VERA.layer1.hscroll = 0;
     VERA.layer1.vscroll = 0;
 
@@ -55,12 +114,13 @@ void initVera()
 
     videomode(VIDEOMODE_40x30);
 
-    for (y = 0; y < GFX_PLANE_HEIGHT; y++)
+    // Initialize both tilemaps to all empty tiles.
+    for (y = 0; y < PLANE_HEIGHT; y++)
     {
-        for (x = 0; x < GFX_PLANE_WIDTH; x++)
+        for (x = 0; x < PLANE_WIDTH; x++)
         {
-            GFX_setBackgroundTile(x, y, 0);
-            GFX_setForegroundTile(x, y, 0);
+            setBackgroundTile(x, y, 0);
+            setForegroundTile(x, y, 0);
         }
     }
 }
@@ -70,7 +130,7 @@ int loadResources()
     int result;
 
     // Load palette
-    result = vload_host(PALBG1_FILENAME, GFX_PALETTE_ADDR);
+    result = vload_host(PALBG1_FILENAME, PALETTE_ADDR);
     if (!result)
 	{
 		printf("  failed to load palette\n");
@@ -78,7 +138,7 @@ int loadResources()
 	}
 
 	// Load background tileset
-    result = vload_host(TSBG1_FILENAME, GFX_BACKGROUND_TILE_BASE_ADDR);
+    result = vload_host(TSBG1_FILENAME, BACKGROUND_TILE_BASE_ADDR);
     if (!result)
 	{
         printf("  failed to load bg tiles\n");
@@ -86,7 +146,7 @@ int loadResources()
     }
 
 	// Load foreground tileset
-    result = vload_host(TSBG1_FILENAME, GFX_FOREGROUND_TILE_BASE_ADDR);
+    result = vload_host(TSBG1_FILENAME, FOREGROUND_TILE_BASE_ADDR);
     if (!result)
 	{
         printf("  failed to load fg tiles\n");
@@ -94,7 +154,7 @@ int loadResources()
     }
 
 	// Load background tilemap to bank
-    result = load_bank_host(TMBG1_FILENAME, GAME_BKG1_MBANK);
+    result = load_bank_host(TMBG1_FILENAME, BKG1_MBANK);
     if (!result) 
 	{
         printf("  failed to load bg tilemap\n");
@@ -102,4 +162,26 @@ int loadResources()
 	}
 
     return 1;
+}
+
+void setBackgroundTile
+    (
+    uint8_t x,
+    uint8_t y,
+    uint16_t tile
+    )
+{
+    vpoke(tile >> 8, (y * PLANE_WIDTH_TIMES_TWO) + (2 * x) + 1 + BACKGROUND_MAP_BASE_ADDR);
+    vpoke(tile & 0xFF, (y * PLANE_WIDTH_TIMES_TWO) + (2 * x) + BACKGROUND_MAP_BASE_ADDR);
+}
+
+void setForegroundTile
+    (
+    uint8_t x,
+    uint8_t y,
+    uint16_t tile
+    )
+{
+    vpoke(tile >> 8, (y * PLANE_WIDTH_TIMES_TWO) + (2 * x) + 1 + FOREGROUND_MAP_BASE_ADDR);
+    vpoke(tile & 0xFF, (y * PLANE_WIDTH_TIMES_TWO) + (2 * x) + FOREGROUND_MAP_BASE_ADDR);
 }
